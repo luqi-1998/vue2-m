2a6ebb9a63982b2b636b0a6bd5a0bcbf11783ec2
luqi-1998 <1277125008@qq.com>
2022-12-18 18:23:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="box">
+    <vue-drag-resize @resizing="resizeEvent" :minw="30" :w="100" :h="50" :minh="30" :parentLimitation="true" :preventActiveBehavior="false" @dragstop="dragEvent">
+      <div :style="styleObj">点击</div>
+    </vue-drag-resize>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueDragResize from "vue-drag-resize";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    VueDragResize
+  },
+  data(){
+    return {
+      styleObj:{
+        'text-align': 'center',
+        'line-height':'50px',
+        'color':'#f00',
+        'background-color':'#eee',
+        'fontSize':'12px',
+        'border-radius':'5px'
+      }
+    }
+  },
+  methods:{
+    dragEvent(e){
+      console.log(e)
+    },
+    resizeEvent(e){
+      this.styleObj['line-height']=e.height+'px'
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.box{
+  height: 300px;
+  width: 500px;
+  position: relative;
+  top: 100px;
+  left: 100px;
+  border: 1px solid #42b983;
 }
 </style>
